981b5449a85a5627afb38fe30ba584d8a859414b
manlakhani2004 <126983888+manlakhani2004@users.noreply.github.com>
2024-01-31 22:04:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/counter';
+
+function App() {
+  return (
+    <div className=" font-bold text-center">
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
